18d6f86466b643be468647a5f96e5ca3da4088ff
mikhailsharaborin <m.sharaborin@paysend.com>
2024-08-03 20:12:20 +0300

    initial app

---
+++ settings.py
@@ -0,0 +1,9 @@
+username = "cam0100"
+password = "cam20240730"
+ip_address = "192.168.0.102"
+port = "554"
+token_telegram = "7159607884:AAG2bYpzRqzdUPM9L9MrlT1XPwEtj7V5mxY"
+date_file = "find_a_new_object.png"
+chat_id = '-4202116069'
+gap_time_second = 20
+url = 'https://api.telegram.org/bot'
